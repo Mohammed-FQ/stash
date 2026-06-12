c418d9f3e4a1a5c5132e26b254ffd2f18f0a4057
Mohammed Alqadda <MohammedFAlqadda@gmail.com>
2026-06-12 15:19:15 +0300

    added simple structure for the project

--- firstProject/main/urls.py
+++ firstProject/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    
+    path('', views.home_view, name="home_view"),
+    path('add/', views.add_view, name="add_view"),
 ]
--- firstProject/main/views.py
+++ firstProject/main/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
+from .models import Team
 # Create your views here.
+
+def home_view(request:HttpRequest):
+    x = 500
+    teams = Team.objects.all()
+    return render(request, 'main/home.html', {'x':x, 'teams': teams})
+
+def add_view(request:HttpRequest):
+    if request.method == 'POST':
+        team_name = request.POST.get('name')
+        team_city = request.POST.get('city')
+        team_wins = request.POST.get('wins')
+        team_losses = request.POST.get('losses')
+        new_team = Team(name=team_name, city=team_city, wins=team_wins, losses=team_losses)
+        new_team.save()
+    return render(request, 'main/add.html')
